bb1489726e5ffb08d691094652c2158f267104a3
JulieGunawan <julie.gunawan@protonmail.com>
2024-01-21 18:04:34 -0700

    toggle button

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 const Tour = (props) => {
   const { id, name, image, price, info, removeTour } = props;
+
+  const [readMore, setReadMore] = useState(false);
+
+  const clickReadMore = () => {
+    setReadMore(!readMore);
+  };
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div>
         <h4>{name}</h4>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 150)}...`}
+          <button type="button" className="btn" onClick={clickReadMore}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
       </div>
       <button
         type="button"
